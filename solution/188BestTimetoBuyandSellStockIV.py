4070d4b74b9b17a13de874ba2f991814d49edfb5
alaric <zrh2014@163.com>
2019-10-24 23:57:13 +0800

    update 188 dp

--- solution/188BestTimetoBuyandSellStockIV.py
+++ solution/188BestTimetoBuyandSellStockIV.py
@@ -20,3 +20,27 @@ class Solution:
                 globalBest[i][j] = max(mustSell[i][j] , globalBest[i-1][j]) 
         
         return globalBest[n-1][k]
+
+
+# dp
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        if n == 0:
+            return 0
+        if k >= n//2:
+            res = 0
+            for i in range(1,n):
+                if prices[i] > prices[i-1]:
+                    res += prices[i] - prices[i-1]
+            return res
+        dp = [ [ [0]*2 for _ in range(k+1)] for _ in range(n) ]
+        for i in range(n):
+            for k in range(1 , k+1):
+                if i == 0 :
+                    dp[i][k][0] = 0
+                    dp[i][k][1] = -prices[i]
+                    continue
+                dp[i][k][0] = max(dp[i-1][k][0] , dp[i-1][k][1] + prices[i])
+                dp[i][k][1] = max(dp[i-1][k][1] , dp[i-1][k-1][0] - prices[i])
+        return dp[n-1][k][0]
